ab3402050b3f9c7dc5db90da7fbfeb154aee6991
tartaric_acid <baka943@qq.com>
2024-05-02 19:44:28 +0800

    修正 E 键关闭界面问题

--- src/main/java/com/github/tartaricacid/netmusic/client/gui/CDBurnerMenuScreen.java
+++ src/main/java/com/github/tartaricacid/netmusic/client/gui/CDBurnerMenuScreen.java
@@ -6,6 +6,7 @@ import com.github.tartaricacid.netmusic.inventory.CDBurnerMenu;
 import com.github.tartaricacid.netmusic.item.ItemMusicCD;
 import com.github.tartaricacid.netmusic.network.NetworkHandler;
 import com.github.tartaricacid.netmusic.network.message.SetMusicIDMessage;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.ChatFormatting;
@@ -161,6 +162,16 @@ public class CDBurnerMenuScreen extends AbstractContainerScreen<CDBurnerMenu> {
             return true;
         }
         return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        InputConstants.Key mouseKey = InputConstants.getKey(keyCode, scanCode);
+        // 防止 E 键关闭界面
+        if (this.getMinecraft().options.keyInventory.isActiveAndMatches(mouseKey) && textField.isFocused()) {
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
     @Override
